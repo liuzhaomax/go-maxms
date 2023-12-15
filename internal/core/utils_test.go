55848bf20611f5cfebcf5ec3dabf54bc89694a73
LIUZHAO <liuzhaomax@163.com>
2023-12-15 20:16:30 +0800

    logger

--- internal/core/utils_test.go
+++ internal/core/utils_test.go
@@ -7,6 +7,41 @@ import (
 	"testing"
 )
 
+func TestIn(t *testing.T) {
+	type input struct {
+		slice   []int
+		element int
+	}
+	cases := []struct {
+		name string
+		have input
+		want bool
+	}{
+		{
+			name: "字符串存在于切片",
+			have: input{
+				slice:   []int{1, 2, 3},
+				element: 2,
+			},
+			want: true,
+		},
+		{
+			name: "字符串不存在于切片",
+			have: input{
+				slice:   []int{1, 2, 3},
+				element: 4,
+			},
+			want: false,
+		},
+	}
+	for _, tc := range cases {
+		got := In(tc.have.slice, tc.have.element)
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, got, tc.want, fmt.Sprintf("\n*** Expected: \n %#v \n*** Got: \n %#v", tc.want, got))
+		})
+	}
+}
+
 func TestGetFuncName(t *testing.T) {
 	cases := []struct {
 		name string
