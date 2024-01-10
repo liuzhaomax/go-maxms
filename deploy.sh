81bd7e531a19e749809d7dee002b026461a38ae1
LIUZHAO <liuzhaomax@163.com>
2024-01-10 14:30:10 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,48 +7,43 @@ version=$4
 container_port=$5
 host_port=$6
 
-containerID=`docker ps -a | grep ${project} | awk "{print $1}"`
+# 确保没有container在运行
+containerID=$(docker ps -a | grep "${project}" | awk '{print $1}')
 
 echo "Container ID: $containerID"
 
 if [ "$containerID" != "" ]; then
-  docker stop $containerID
-  docker rm $containerID
+  docker stop "$containerID"
+  docker rm "$containerID"
 fi
 
-imageName=$harbor_addr/$harbor_repo/$project:$version
+# 确保没有同名image
+imageName="$harbor_addr/$harbor_repo/$project:$version"
 
 echo "Image Name: $imageName"
 
-tag=`docker images | grep ${project} | awk "{print $2}"`
+tag=$(docker images | grep "${project}" | awk '{print $2}')
 
 echo "Image Tag: $tag"
 
 # shellcheck disable=SC2039
-if [[ $tag =~ $version ]]; then
-  docker rmi $imageName
+if [ "$tag" != "$version" ]; then
+  docker rmi "$imageName"
 fi
 
-docker login -u admin -p Harbor12345 $harbor_addr
+# 登录harbor
+docker login -u admin -p Harbor12345 "$harbor_addr"
 
-docker pull $imageName
-
-
-# shellcheck disable=SC2039
-# shellcheck disable=SC2053
-if [[ $tag == $version ]]; then
-  docker stop project
-  docker rm project
-fi
+docker pull "$imageName"
 
 docker run \
-  --name=${project} \
+  --name="$project" \
   -d \
   --restart=always \
   --privileged=true \
-  -p ${host_port}:${container_port} \
+  -p "${host_port}:${container_port}" \
   -v /root/www:/usr/src/app/www \
-  /root/logs/${project}/golog.txt:/usr/src/app/golog.txt \
-  ${imageName}
+  -v /root/logs/"${project}"/golog.txt:/usr/src/app/golog.txt \
+  "$imageName"
 
 echo "SUCCESS: Container Created"
