394d6f90d92c6f6c0a01d9c6f572429427622d57
LIUZHAO <liuzhaomax@163.com>
2024-04-18 08:45:51 +0800

    gen

--- spec/gen.sh
+++ spec/gen.sh
@@ -3,8 +3,8 @@
 CONTRACT_PATH=user.proto
 OUT_PATH=../src/api_user_rpc/pb
 
-mkdir -p ${OUT_PATH}
 cd spec
+mkdir -p ${OUT_PATH}/
 protoc --go_out=${OUT_PATH} \
   --go_opt=paths=source_relative \
   --go-grpc_out=require_unimplemented_servers=false:${OUT_PATH} \
