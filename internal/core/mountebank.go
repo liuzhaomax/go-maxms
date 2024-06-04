967d760406a94c15f6efda301820913bd921322f
LIUZHAO <liuzhaomax@163.com>
2024-06-04 17:14:12 +0800

    mountebank stub

--- internal/core/mountebank.go
+++ internal/core/mountebank.go
@@ -36,6 +36,7 @@ func (m *Mountebank) CreateImposter(stubDir string) {
 	if err := v.Unmarshal(&imposterMap); err != nil {
 		log.Fatalf("解析imposter为map失败: %v", err)
 	}
+	stubPort := imposterMap["port"]
 	// 序列化 map 为 JSON
 	imposterJSON, err := json.Marshal(imposterMap)
 	if err != nil {
@@ -47,11 +48,44 @@ func (m *Mountebank) CreateImposter(stubDir string) {
 	// 发送请求
 	resp, err := http.Post(mbURL, contentType, bytes.NewBuffer(imposterJSON))
 	if err != nil {
-		log.Fatalf("发送imposter失败: %v", err)
+		log.Fatalf("发送请求失败: %v", err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusCreated {
 		log.Fatalf("创建imposter失败, status code: %v", resp.StatusCode)
 	}
-	log.Println("imposter创建成功")
+	log.Printf("imposter创建成功，stub运行在端口：%v", stubPort)
+}
+
+func (m *Mountebank) DeleteImposter(stubDir string) {
+	// 读取imposter请求体
+	v := viper.New()
+	v.SetConfigFile(stubDir)
+	err := v.ReadInConfig()
+	if err != nil {
+		log.Fatalf("imposter json 读取错误: %v", err)
+	}
+	// 将读取的配置转换为 map[string]interface{}
+	var imposterMap map[string]interface{}
+	if err := v.Unmarshal(&imposterMap); err != nil {
+		log.Fatalf("解析imposter为map失败: %v", err)
+	}
+	stubPort := imposterMap["port"]
+	// 请求的其他参数
+	mbURL := fmt.Sprintf("%s://%s:%s/imposters", cfg.Lib.Mountebank.Protocol, cfg.Lib.Mountebank.Mb.Endpoint.Host, cfg.Lib.Mountebank.Mb.Endpoint.Port)
+	// 发送请求
+	req, err := http.NewRequest("DELETE", mbURL, nil)
+	if err != nil {
+		log.Fatalf("请求创建失败: %v", err)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("发送请求失败: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("删除imposter失败, status code: %v", resp.StatusCode)
+	}
+	log.Printf("位于%v端口的imposter删除成功\n", stubPort)
 }
