5d95e4f20c01867adebbe418d34564420b304ca5
LIUZHAO <liuzhaomax@163.com>
2024-06-06 18:39:34 +0800

    mountebank stub

--- test/imposter/imposter_test.go
+++ test/imposter/imposter_test.go
@@ -14,23 +14,7 @@ const stubDir = "../stub/imposter.json"
 const ENV = "dev"
 
 func TestUpdateImposter(t *testing.T) {
-	v := viper.New()
-	cfg := core.GetConfig()
-	v.AutomaticEnv()
-	env := flag.String("e", ENV, "环境")
-	flag.Parse() // 后面有*env，必须先解析
-	configFile := flag.String("c", fmt.Sprintf("%s/%s.yaml", configDir, *env), "配置文件")
-	flag.Parse()
-	// 读取Config
-	v.SetConfigFile(*configFile)
-	err := v.ReadInConfig()
-	if err != nil {
-		log.Fatalf("读取配置文件时出错: %v", err)
-	}
-	err = v.Unmarshal(cfg)
-	if err != nil {
-		log.Fatalf("解析配置文件时出错: %v", err)
-	}
+	cfg := getConfig()
 	// 删除imposter
 	cfg.DeleteImposter(stubDir)
 	// 创建imposter
@@ -38,28 +22,18 @@ func TestUpdateImposter(t *testing.T) {
 }
 
 func TestCreateImposter(t *testing.T) {
-	v := viper.New()
-	cfg := core.GetConfig()
-	v.AutomaticEnv()
-	env := flag.String("e", ENV, "环境")
-	flag.Parse() // 后面有*env，必须先解析
-	configFile := flag.String("c", fmt.Sprintf("%s/%s.yaml", configDir, *env), "配置文件")
-	flag.Parse()
-	// 读取Config
-	v.SetConfigFile(*configFile)
-	err := v.ReadInConfig()
-	if err != nil {
-		log.Fatalf("读取配置文件时出错: %v", err)
-	}
-	err = v.Unmarshal(cfg)
-	if err != nil {
-		log.Fatalf("解析配置文件时出错: %v", err)
-	}
+	cfg := getConfig()
 	// 创建imposter
 	cfg.CreateImposter(stubDir)
 }
 
 func TestDeleteImposter(t *testing.T) {
+	cfg := getConfig()
+	// 删除imposter
+	cfg.DeleteImposter(stubDir)
+}
+
+func getConfig() *core.Config {
 	v := viper.New()
 	cfg := core.GetConfig()
 	v.AutomaticEnv()
@@ -77,6 +51,5 @@ func TestDeleteImposter(t *testing.T) {
 	if err != nil {
 		log.Fatalf("解析配置文件时出错: %v", err)
 	}
-	// 删除imposter
-	cfg.DeleteImposter(stubDir)
+	return cfg
 }
