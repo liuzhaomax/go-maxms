5da476683a5b6cd9fc273afbe2c633b101122ae9
LIUZHAO <liuzhaomax@163.com>
2024-07-27 18:10:42 +0800

    max multipart memory

--- internal/core/gin.go
+++ internal/core/gin.go
@@ -11,12 +11,14 @@ func init() {
 }
 
 type Gin struct {
-	RunMode string `mapstructure:"run_mode"`
+	RunMode            string `mapstructure:"run_mode"`
+	MaxMultipartMemory int64  `mapstructure:"max_multipart_memory"`
 }
 
 // InitGinEngine Gin引擎的provider
 func InitGinEngine() *gin.Engine {
-	gin.SetMode(GetConfig().Lib.Gin.RunMode) // debug, test, release
+	gin.SetMode(cfg.Lib.Gin.RunMode) // debug, test, release
 	app := gin.Default()
+	app.MaxMultipartMemory = cfg.Lib.Gin.MaxMultipartMemory << 20
 	return app
 }
